2719f7b8bc9495e659b3a0d9110fc8adb89b672e
Robert Jäckel <rjaeckel@users.noreply.github.com>
2023-12-02 23:39:01 +0100

    Day 1 and 2

---
+++ 1.ps1
@@ -0,0 +1,32 @@
+Function Get-CalibrationValue {
+    [CmdletBinding()]param(
+        [Parameter(ValueFromPipeline=$true)][String]$Line
+    )
+    begin {
+        $Value = 0
+        $Capture = '(\d|one|two|three|four|five|six|seven|eight|nine)'
+    }
+    process {
+        $start = $Line -replace "^[^\d]*?$Capture.*", '$1'
+        $end   = $Line -replace ".*$Capture[^\d]*`$", '$1'
+
+        $start, $end = $start, $end -replace 'one',   '1' `                                    -replace 'two',   '2' `
+                                    -replace 'three', '3' `
+                                    -replace 'four',  '4' `
+                                    -replace 'five',  '5' `
+                                    -replace 'six',   '6' `                                    -replace 'seven', '7' `
+                                    -replace 'eight', '8' `
+                                    -replace 'nine',  '9'
+        
+        $LineValue= 0+"$start$end"
+        #"$Line $LineValue"
+        $Value+=$LineValue
+    }
+    end { $Value }
+}
+
+$Demo = "two1nine|eightwothree|abcone2threexyz|xtwone3four|4nineeightseven2|zoneight234|7pqrstsixteen"
+
+$Demo -split '\|' | Get-CalibrationValue
+
+Get-Content "$PSScriptRoot\Input-1.txt" | Get-CalibrationValue
---
+++ 2.ps1
@@ -0,0 +1,35 @@
+﻿Function Test-Games {
+    [CmdletBinding()]param(
+        [Parameter(ValueFromPipeline=$true)][String]$Game
+    )
+    Begin {
+        $SumValidIDs = 0;
+        $SumPowers = 0;
+    }
+    Process {
+        [int]$GameId,[string]$Grabbed = $Game -replace '^Game ','' -split ': '
+        $max = @{'red'=0; 'green'=0; 'blue'=0 }
+        $Grabbed -split '; ' | % {
+            $_ -split ', ' |% {
+                [int]$Count,[string]$Color = $_ -split ' ';
+                if ($Count -gt $max[$Color]) { $max[$Color] = $Count }
+            }
+        }
+        If (($max['red'] -le 12) -and ($max['green'] -le 13) -and ($max['blue'] -le 14)) {
+            $SumValidIDs+= $GameId
+        }
+        $SumPowers += $max['red']*$max['green']*$max['blue']
+    }
+    End { "$SumValidIDs $SumPowers" }
+}
+
+$Demo = @"
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"@
+
+$Demo -split '\r\n' | Test-Games
+Get-Content "$PSScriptRoot\Input-2.txt" | Test-Games
