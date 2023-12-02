b2d60d71bffbafd6c429e65bc2b18cda6d50d5db
Robert Jäckel <rjaeckel@users.noreply.github.com>
2023-12-03 00:44:20 +0100

    Pretty up 1

--- 1.ps1
+++ 1.ps1
@@ -7,26 +7,26 @@ Function Get-CalibrationValue {
         $Capture = '(\d|one|two|three|four|five|six|seven|eight|nine)'
     }
     process {
-        $start = $Line -replace "^[^\d]*?$Capture.*", '$1'
-        $end   = $Line -replace ".*$Capture[^\d]*`$", '$1'
+        $start = $Line -replace ('.*?{0}.*' -F$Capture), '$1'
+        $end = $Line -replace ('.*{0}.*' -F$Capture), '$1'
 
-        $start, $end = $start, $end -replace 'one',   '1' `                                    -replace 'two',   '2' `
+        $start, $end = $start, $end -replace 'one',   '1' `
+                                    -replace 'two',   '2' `
                                     -replace 'three', '3' `
                                     -replace 'four',  '4' `
                                     -replace 'five',  '5' `
-                                    -replace 'six',   '6' `                                    -replace 'seven', '7' `
+                                    -replace 'six',   '6' `
+                                    -replace 'seven', '7' `
                                     -replace 'eight', '8' `
                                     -replace 'nine',  '9'
         
-        $LineValue= 0+"$start$end"
-        #"$Line $LineValue"
+        [int]$LineValue="$start$end"
         $Value+=$LineValue
     }
     end { $Value }
 }
 
 $Demo = "two1nine|eightwothree|abcone2threexyz|xtwone3four|4nineeightseven2|zoneight234|7pqrstsixteen"
-
 $Demo -split '\|' | Get-CalibrationValue
 
 Get-Content "$PSScriptRoot\Input-1.txt" | Get-CalibrationValue
